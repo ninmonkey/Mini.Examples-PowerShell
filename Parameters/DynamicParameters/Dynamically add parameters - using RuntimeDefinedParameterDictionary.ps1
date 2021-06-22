c3740058aa8eeaa51cdce4545e7678f2ec0c9e90
Jake Bolton <ninmonkeys@gmail.com>
2021-06-21 22:09:57 -0500

    New: DynamicParameters demo

---
+++ Parameters/DynamicParameters/Dynamically add parameters - using RuntimeDefinedParameterDictionary.ps1
@@ -0,0 +1,81 @@
+using namespace System.Management.Automation
+using namespace System.Collections
+<#
+Refs:
+
+[IValidateSetValuesGenerator](https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_functions_advanced_parameters?view=powershell-7.2#dynamic-parameters)
+
+[DynamicParam block](https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_functions_advanced_parameters?view=powershell-7.2#dynamic-validateset-values)
+
+    DynamicParam {<statement-list>}
+
+
+additional
+    [(https://docs.microsoft.com/en-us/dotnet/api/system.management.automation.idynamicparameters.getdynamicparameters?view=powershellsdk-7.0.0#System_Management_Automation_IDynamicParameters_GetDynamicParameters)
+    (https://docs.microsoft.com/en-us/dotnet/api/system.management.automation.idynamicparameters?view=powershellsdk-7.0.0)
+    (https://docs.microsoft.com/en-us/dotnet/api/system.management.automation.runtimedefinedparameter?view=powershellsdk-7.0.0)
+
+## notes
+
+[RuntimeDefinedParameter]:
+    - attributes of parameter such as (mandatory, postion, ValuefromPipeline)
+
+## see:
+
+    [System.Management.Automation.RuntimeDefinedParameter] | find-member -MemberType Constructor | ft -AutoSize
+    [System.Management.Automation.ParameterAttribute] | find-member -MemberType Constructor | ft -AutoSize
+    [System.Management.Automation.IValidateSetValuesGenerator]
+#>
+function New-Animal {
+    <#
+    .synopsis
+        Only cats will show the dynamic parameter -Lives
+    .description
+        the variable name '$dynParamLives' is used to access the dynamic value
+    .parameter Species
+        [string] : one of the valid set
+    .parameter Lives
+        [int32] : Dynamically added when species is a cat
+    .example
+        PS> New-Animal -Species 'Dog'
+        PS> New-Animal -Species 'Cat' -Lives 9
+
+    #>
+    [cmdletbinding(PositionalBinding = $false)]
+    param(
+        [ValidateSet('Cat', 'Dog', 'Other')]
+        [parameter(Mandatory, Position = 0)]
+        [string]$Species = 'Other'
+    )
+
+    DynamicParam {
+        if ($Species -eq 'Cat') {
+            $pAttr = [ParameterAttribute]::new()
+            $pAttr.ParameterSetName = 'SubtypeCat'
+            $pAttr.Mandatory = $true
+            $pAttr.HelpMessage = '?help Messege'      # isn't accessable afaik
+            $pAttr.HelpMessageBaseName = '?help Base' # isn't accessable afaik
+            $pAttr.Position = 1
+
+            $attrList = [Collections.ObjectModel.Collection[Attribute]]::new()
+            $attrList.Add( $pAttr )
+
+
+            $dynParamLives = [RuntimeDefinedParameter]::new('Lives', [int32], $attrList)
+
+            $paramDict = [RuntimeDefinedParameterDictionary]::new()
+            $paramDict.Add( 'Lives', $dynParamLives )
+
+            return $paramDict
+        }
+    }
+
+    process {
+        $lives = ($dynParamLives)?.Value ?? 1
+        $animal = [pscustomobject]@{
+            'Species' = $Species
+            'Lives'   = $Lives
+        }
+        $animal
+    }
+}
