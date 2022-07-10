b14ea37a03bf75b55d834263c6647eb9ba01580b
Jake Bolton <ninmonkeys@gmail.com>
2022-07-10 13:54:00 -0500

    iter

--- notebooks/web-parsing/power-html.buff.ps1
+++ notebooks/web-parsing/power-html.buff.ps1
@@ -13,6 +13,8 @@ todo:
 
     - [ ] update type `[HtmlAgilityPack.HtmlNode]` to have a max lengths
         for `.OuterHtml` and probably `.InnerHtml`, `OuterText, and  `InnerText`
+    - [ ] pipe to FZF with XPATH
+        that XPATH renders the actual sub-document that
 
 see:
     - [HtmlAgility docs TOC](https://html-agility-pack.net/documentation)
@@ -50,16 +52,143 @@ $q.Div_IdList.value | Sort-Object | UL | Label 'all IDs set on Div elements'
 $q.Tables = $doc.SelectNodes('//table')
 
 
+function _exportNodeAsHtml {
+    param( $Source,
+        [switch]$AutoOpen
+    )
+    # $innerHtml = $q.Tables[0].InnerHtml | Select-Object -First 5
+    # $rawHtml = $q.Tables[0].InnerHtml
+    $Html = $Source
+
+    $RenderedHtml = @'
+<html><body>
+<div id="main-content">
+{0}
+</div>
+</body></html>
+'@ -f @(
+        $Html
+    )
+
+    $RenderedHtml | Sc -Path 'temp:\export-outer.html'
+    $dest = Get-Item 'temp:\export-outer.html'
+    Label 'Wrote' $Dest.FullName #| Write-Information
+
+    # Get-Item $dest | GoCode
+}
 
 
+$innerHtml = $q.Tables[0].InnerHtml | Select-Object -First 5
+$rawHtml = $q.Tables[0].InnerHtml
+$SomeTarget = $q.Tables[0]
+_exportNodeAsHtml $SomeTarget.OuterHtml
 
-
-
-
+function _fmtTable_HAP_htmlAttribute {
+    # Summarize a [HHtmlAgilityPack.HtmlAttribute]
+    [CmdletBinding()]
+    param(
+        [Parameter(ValueFromPipeline)]$InputObject
+    )
+    begin {
+        $Items = [list[object]]::new()
+    }
+    process {
+        foreach ($obj in $InputObject) {
+            $Items.Add( $obj )
+        }
+    }
+    end {
+        if (@($items)[0] -isnot 'HtmlAgilityPack.HtmlAttribute') {
+            Write-Debug "UnexpectedTypeWarning: $($InputObject.GetType())"
+        }
+        $items
+        | Sort-Object Value
+        | Format-Table Value, DeEntitizeValue, XPath
+    }
+}
+function _fmtTable_HAP_htmlNode {
+    # Summarize a [HHtmlAgilityPack.HtmlAttribute]
+    [CmdletBinding()]
+    param(
+        [Parameter(ValueFromPipeline)]$InputObject
+    )
+    begin {
+        $Items = [list[object]]::new()
+    }
+    process {
+        foreach ($obj in $InputObject) {
+            $Items.Add( $obj )
+        }
+    }
+    end {
+        if (@($items)[0] -isnot 'HtmlAgilityPack.HtmlNode') {
+            Write-Debug "UnexpectedTypeWarning: $($InputObject.GetType())"
+        }
+        $PropsForAttribute = 'Name', 'Attributes', 'ClosingAttributes', 'EndNode', 'HasAttributes', 'HasChildNodes', 'XPath', 'InnerText', 'InnherHtml'
+        $items
+        | Sort-Object Name
+        | Select-Object $PropsForAttribute
+        | Format-Table
+    }
+}
+H1 'different defaults: htmlAttribute'
+$q.Div_IdList | _fmtTable_HAP_htmlAttribute
+H1 'htmlNode'
+$q.Tables[0] | _fmtTable_HAP_htmlNode
 
 # Import-Module PowerHTML
 # $q = PowerHTML\ConvertFrom-Html -URI 'https://powerbi.microsoft.com/en-us/blog/'
+_exportNodeAsHtml $SomeTarget.OuterHtml
+$q.tables | s -ExcludeProperty inner*, outer*
 
+$q.tables[0] | s -ExcludeProperty inner*, outer* | s -First 1
+
+$Target = $q.Tables[0]
+$render1 = @'
+[{5} ] name: {0}
+childrenTypesOf: {6}
+HasChild = {1} HasAttrib = {2} HasClosingAttr = {3}
+overflow: {4}
+
+{5}
+
+{6}
+
+'@ -f @(
+    # 0
+    $Target.Name ?? 'missing'
+
+    # 1,2,3
+    $target.HasChildNodes ?? 'missing'
+    $target.HasAttributes ?? 'missing'
+    $target.HasClosingAttributes ?? 'missing'
+
+    # 4
+    $Target.NodeType ?? 'missing'
+    #5
+
+    $Target.ChildNodes | ForEach-Object pstypenames | ForEach-Object { $_ -as 'type' }
+    | shortType | Sort-Object -Unique
+    | Join-String -Separator ' ' -op "${fg:green}PSTypenames ${fg:reset}"
+
+    #6
+    $Target.ChildNodes
+    | Join-String { $_.NodeType, $_.Name -join '=' } -sep ',  ' -op 'childNodes = @(' -os ')'
+    | Join-String -op "${fg:orange}ChildNodes = " -os "${fg:clear}"
+    | Join-String -op "${fg:green}ChildNodes> " -os "${fg:clear}"
+    #old:
+    # $Target.ChildNodes | Join-String { $_.NodeType, $_.Name -join '=' } -sep ',  ' -op 'childNodes = @(' -os ')'
+    # | Join-String -op "${fg:gray80}ChildNodes = " -os "${fg:clear}"
+)
+
+$render1
+
+
+
+'open live preview of (_exportNodeAsHtml)''s path' | Label 'Tip' -fg green
+| Write-Warning
+
+$noop = $Null
 # # find all links on the page
 # $q.SelectNodes("//a")
 
