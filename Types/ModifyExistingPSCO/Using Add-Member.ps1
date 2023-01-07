7088f2cc2c506d1cc9868ee1e984764a75329d87
Jake Bolton <ninmonkeys@gmail.com>
2023-01-06 19:54:55 -0600

    Create Using Add-Member.ps1

---
+++ Types/ModifyExistingPSCO/Using Add-Member.ps1
@@ -0,0 +1,26 @@
+Import-Module  NameIt
+
+<#
+This example adds functions to objects you are piping from
+
+- this modifies an object
+- the other one modifies everything that uses a type
+
+#>
+
+function GetUser {
+     [pscustomobject]@{
+        name = Ig '[lastname]'
+        Id = Ig '####'
+     }
+ }
+
+ ($someUser = GetUser)
+ | Add-Member -MemberType ScriptProperty -Name 'username' -Value {
+   '{0}_{1}' -f @(   $this.Name , $this.Id )
+ } -PassThru
+
+$someUser.id = '🐵'
+
+# 'after changing .id, the other property is affected'
+$someUser
